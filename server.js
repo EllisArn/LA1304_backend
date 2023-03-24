64370ccdd890aec6c661a755c41bd922b374b7c5
Ellis Arn <89130718+EllisArn@users.noreply.github.com>
2023-03-24 09:45:05 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,84 @@
+const mongoose = require('mongoose')
+const cors = require('cors')
+const express = require('express')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const app = express()
+
+const datenSchema = new mongoose.Schema({
+  kontinent: String,
+  land: String,
+  hauptstadt: String,
+})
+
+const Welt = mongoose.model('country', datenSchema)
+
+app.use(cors())
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(`${process.env.DB_URL}`).then(() => {
+  console.log('Connected to database')
+})
+
+app.get('/africa', (req, res) => {
+  Welt.find(
+    { Kontinent: 'Afrika' },
+    { Land: 1, Hauptstadt: 1, _id: 0 },
+    (err, data) => {
+      res.json(data)
+    }
+  ).catch((err) =>
+    console.log('Fehler beim Abrufen von Daten aus der Datenbank:', err)
+  )
+})
+
+app.get('/america', (req, res) => {
+  Welt.find(
+    { Kontinent: 'Amerika' },
+    { Land: 1, Hauptstadt: 1, _id: 0 },
+    (err, data) => {
+      res.json(data)
+    }
+  ).catch((err) =>
+    console.log('Fehler beim Abrufen von Daten aus der Datenbank:', err)
+  )
+})
+
+app.get('/asia', (req, res) => {
+  Welt.find(
+    { Kontinent: 'Asien' },
+    { Land: 1, Hauptstadt: 1, _id: 0 },
+    (err, data) => {
+      res.json(data)
+    }
+  ).catch((err) =>
+    console.log('Fehler beim Abrufen von Daten aus der Datenbank:', err)
+  )
+})
+
+app.get('/europe', (req, res) => {
+  Welt.find(
+    { Kontinent: 'Europa' },
+    { Land: 1, Hauptstadt: 1, _id: 0 },
+    (err, data) => {
+      res.json(data)
+    }
+  ).catch((err) =>
+    console.log('Fehler beim Abrufen von Daten aus der Datenbank:', err)
+  )
+})
+
+app.get('/world', (req, res) => {
+  Welt.find({}, { Land: 1, Hauptstadt: 1, _id: 0 }, (err, data) => {
+    res.json(data)
+  }).catch((err) =>
+    console.log('Fehler beim Abrufen von Daten aus der Datenbank:', err)
+  )
+})
+
+app.listen(3000, () => {
+  console.log(`Example app listening on port 3000`)
+})
